b21bea5a82b4f9fa396eef695cc389bcce50d858
Eduardo Junior <edujrrib@gmail.com>
2016-08-13 17:42:42 -0300

    Teste compilar tex com bash para debugar erro no travis

--- _render.R
+++ _render.R
@@ -2,11 +2,11 @@
 ## Iscas para verificar o erro no Travis
 cat("## @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ##",
     capture.output(sessionInfo()),
-    capture.output(installed.packages()),
+    ## capture.output(installed.packages()),
     capture.output(dir()),
     "## @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ##",
     sep = "\n")
 
 ## Compile os documentos
-## sapply(grep('.Rnw$', dir(), value = TRUE), knitr::knit)
-knitr::knit2pdf("teste.Rnw")
+sapply(grep('.Rnw$', dir(), value = TRUE), knitr::knit)
+## knitr::knit2pdf("teste.Rnw")
